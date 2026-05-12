d8511a3a8bc0204e6028931415526d2cba9aabe6
sanchedie <66399984+sanchedie@users.noreply.github.com>
2026-05-11 20:19:46 -0500

    Add simple interest calculator in calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+
+capital = float(input("Ingrese el capital: "))
+tasa = float(input("Ingrese la tasa: "))
+tiempo = float(input("Ingrese el tiempo: "))
+
+interes = capital * tasa * tiempo
+
+print("El interés es:", interes)
